3ff5de48aa47c5637639aba93efc2d3f564b4166
wangfan <i.wangfan@foxmail.com>
2017-09-30 23:14:35 +0800

    update

--- front-end/JDFastMenu/index.js
+++ front-end/JDFastMenu/index.js
@@ -1,12 +1,35 @@
 $(function(){
     var sub = $('.subMenuContainer')
 
-    $('.mainMenu')
+    var activeRow, activeMenu
+    $('.wrapper')
         .mouseenter(function(e) {
             sub.removeClass('none')
         })
         .mouseleave(function(e) {
             sub.addClass('none')
+            if (activeRow) {
+                activeRow.removeClass('active')
+                activeRow = null
+            }
+
+            if (activeMenu) {
+                activeMenu.addClass('none')
+                activeMenu = null
+            }
+
+        })
+        .on('mouseenter', '.mMenu-item' , function(e) {
+            if (activeRow === $(e.target)) return
+
+            if (activeRow) {
+                activeRow.removeClass('active')
+                activeMenu.addClass('none')
+            }
+
+            activeRow = $(e.target).addClass('active')
+            activeMenu = $('.subMenuContainer [data-id='+activeRow.data('id')+']')
+            activeMenu.removeClass('none')
         })
         
 
